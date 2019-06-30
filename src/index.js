d7aba7aa0bd9b466175c346cc0c36c3e800b77c4
Dheeraj Vepakomma <dheerajvs@gmail.com>
2019-06-30 21:32:51 +0530

    use bulma carousel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
 require('./styles.scss');
-const bulmaQuickview = require('../node_modules/bulma-quickview/dist/js/bulma-quickview.min.js');
+
+const bulmaCarousel = require('../node_modules/bulma-carousel/dist/js/bulma-carousel');
+const bulmaQuickview = require('../node_modules/bulma-quickview/dist/js/bulma-quickview');
+
+bulmaCarousel.attach('.slider-container', {
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  breakpoints: [
+    {
+      changePoint: 480,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+    {
+      changePoint: 640,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    },
+    {
+      changePoint: 768,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+    },
+    {
+      changePoint: 1023,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+    {
+      changePoint: 1216,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    },
+  ],
+});
 
 bulmaQuickview.attach();
