56cda36bf47c7a9539ef61799364e057b153f0d8
Dheeraj Vepakomma <dheerajvs@gmail.com>
2019-07-13 21:14:55 +0530

    use workbox-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -35,3 +35,16 @@ bulmaCarousel.attach('.slider-container', {
 });
 
 bulmaQuickview.attach();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,97 +4,89 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
-const rules = {
-  js: {
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
+module.exports = {
+  entry: {
+    build: './src/build.js',
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components/'),
+      pages: path.resolve(__dirname, 'src/pages/'),
     },
   },
-  sass: {
-    test: /\.s[ac]ss$/,
-    use: [
-      MiniCssExtractPlugin.loader,
+  module: {
+    rules: [
       {
-        loader: 'css-loader',
-      },
-      {
-        loader: 'resolve-url-loader',
-      },
-      {
-        loader: 'sass-loader',
-        options: {
-          sourceMap: true,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'resolve-url-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
       },
     ],
   },
-  fonts: {
-    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-    use: [
-      {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'fonts/',
-        },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([{ from: '**/*', to: '', context: 'src/static/' }]),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new StaticSiteGeneratorPlugin({
+      entry: 'build',
+      globals: {
+        window: {},
       },
-    ],
-  },
+      paths: ['/', '/about/', '/stories/', '/asha-deep/'],
+    }),
+    new GenerateSW({
+      importWorkboxFrom: 'local',
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      open: false,
+      port: 8080,
+      server: { baseDir: ['dist'] },
+    }),
+  ],
 };
-
-module.exports = [
-  {
-    entry: {
-      build: './src/build.js',
-    },
-    output: {
-      filename: '[name].js',
-      libraryTarget: 'umd',
-    },
-    resolve: {
-      alias: {
-        components: path.resolve(__dirname, 'src/components/'),
-        pages: path.resolve(__dirname, 'src/pages/'),
-      },
-    },
-    module: {
-      rules: [rules.js],
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new StaticSiteGeneratorPlugin({
-        entry: 'build',
-        globals: {
-          window: {},
-        },
-        paths: ['/', '/about/', '/stories/', '/asha-deep/'],
-      }),
-      new BrowserSyncPlugin({
-        host: 'localhost',
-        open: false,
-        port: 8080,
-        server: { baseDir: ['dist'] },
-      }),
-    ],
-  },
-  {
-    entry: {
-      index: './src/index.js',
-    },
-    output: {
-      filename: '[name].js',
-    },
-    module: {
-      rules: [rules.js, rules.sass, rules.fonts],
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-      }),
-      new CopyPlugin([{ from: '**/*', to: '', context: 'src/static/' }]),
-    ],
-  },
-];
